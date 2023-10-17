6c4f45a8293966d56cc92bfe70d2403ba7d57299
tywest121 <tyresewest25@gmail.com>
2023-10-17 17:03:49 -0400

    Dude

---
+++ app.js
@@ -0,0 +1,24 @@
+const ourArray = [];{
+    for (let i = 1; i < 10; i++) {
+      ourArray.push(i);
+    }}
+
+const myArray = [];{
+    let i = 1;
+    while(i < 10){
+        myArray.push(i);
+        i++;
+    }
+}    
+
+const yourArray = [];{
+    for (let i = 11; i < 21; i += 2){
+        yourArray.push(i);
+    }
+}
+
+const mineArray = [];{
+    for (let i = 15; i > 1; i -= 2){
+        mineArray.push(i);
+    }
+}
